53c46b97b6c498c14edd3a920b9ea8797428a503
Dmitry Sadovnychyi <git@dmit.ro>
2015-03-10 22:57:02 +0800

    Fix typos

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ import setuptools
 
 setuptools.setup(
   name='gae-console',
-  description=('Powerfull Interactive Console for App Engine applications'),
+  description=('Powerful Interactive Console for App Engine applications.'),
   version='0.0.1',
   packages=setuptools.find_packages(),
   author='Dmitry Sadovnychyi',
   author_email='gae-console@dmit.ro',
   keywords=['google app engine', 'gae', 'appengine', 'console', 'terminal'],
   url='https://github.com/sadovnychyi/gae-console',
-  license='Apache License 2.0',
+  license='GNU General Public License v2.0',
   include_package_data=True,
   exclude_package_data={'': ['README.md']},
 )
